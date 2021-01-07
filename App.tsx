ae30b7179b96feeff74ef35cff567ff12ee7ceaf
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-08 00:33:47 +0200

    done creating component to load assets

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { LoadAssets } from "./components";
+
+const fonts = {
+  Bold: require("./assets/fonts/OpenSans-Bold.ttf"),
+  Regular: require("./assets/fonts/OpenSans-Regular.ttf"),
+};
 
 export default function App() {
   return (
