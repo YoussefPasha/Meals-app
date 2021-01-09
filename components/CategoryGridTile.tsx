6da0d8105c6cf74dddf06c5c280171d267bbe0ea
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-09 12:41:42 +0200

    Grid Styling & Some Refactoring

---
+++ components/CategoryGridTile.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  title: {
+    fontFamily: "bold",
+    fontSize: 20,
+    textAlign: "right",
+  },
+});
+
+const CategoryGridTile = (props: any) => {
+  let TouchableComponent: any;
+  TouchableComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComponent = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.gridItem}>
+      <TouchableComponent style={{ flex: 1 }} onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.title}>{props.title}</Text>
+        </View>
+      </TouchableComponent>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,41 +1,20 @@
 import React from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
-  },
-});
+import { CategoryGridTile } from "../components";
 
 const CategoriesScreen = (props: any) => {
   const renderGridItem = (itemData: any) => {
     return (
-      <TouchableOpacity
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate("CategoryMealsScreen", {
             categoryId: itemData.item.id,
           });
         }}
-        style={styles.gridItem}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
   return (
