d3c8073fe56ab2b2f8c773789233f06620376071
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-11 06:31:18 +0200

    create drawer navigator

--- navigation/MealsNavigator.tsx
+++ navigation/MealsNavigator.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from "expo-status-bar";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
@@ -12,6 +13,7 @@ import MealDetailScreen from "../screens/MealDetailScreen";
 import { LoadAssets } from "../components";
 import colors from "../constants/colors";
 import FavoritesScreen from "../screens/FavoritesScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 
 const fonts = {
   regular: require("../assets/fonts/OpenSans-Regular.ttf"),
@@ -91,54 +93,77 @@ const Tab: any =
     ? createMaterialBottomTabNavigator()
     : createBottomTabNavigator();
 
-export default function MealsTabNavigator() {
+const MealsTab = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="white"
+      shifting={true}
+      tabBarOptions={{
+        activeTintColor: colors.primaryColor,
+        inactiveTintColor: "gray",
+        style: {
+          backgroundColor: "#E1BEE7",
+        },
+        labelStyle: {
+          fontSize: 14,
+          fontFamily: "bold",
+        },
+      }}
+      screenOptions={({ route }: any) => ({
+        tabBarIcon: ({ focused, color }: any) => {
+          let iconName: any;
+
+          if (route.name === "Home") {
+            iconName = focused ? "ios-restaurant" : "ios-restaurant-outline";
+          } else if (route.name === "Favorites") {
+            iconName = focused ? "ios-star" : "ios-star-outline";
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={20} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={AuthMealsNavigator}
+        options={{
+          tabBarColor: colors.primaryColor,
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavNavigator}
+        options={{
+          tabBarColor: colors.accentColor,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const FilterStack = createStackNavigator();
+
+const FilterNav = () => {
+  return (
+    <FilterStack.Navigator>
+      <FilterStack.Screen
+        name="FilterScreen"
+        component={FiltersScreen}
+      ></FilterStack.Screen>
+    </FilterStack.Navigator>
+  );
+};
+
+const Drawer = createDrawerNavigator();
+export default function MainNavigator() {
   return (
     <LoadAssets {...{ fonts }}>
       <StatusBar style="dark" />
-      <Tab.Navigator
-        initialRouteName="Home"
-        activeColor="white"
-        shifting={true}
-        tabBarOptions={{
-          activeTintColor: colors.primaryColor,
-          inactiveTintColor: "gray",
-          style: {
-            backgroundColor: "#E1BEE7",
-          },
-          labelStyle: {
-            fontSize: 14,
-            fontFamily: "bold",
-          },
-        }}
-        screenOptions={({ route }: any) => ({
-          tabBarIcon: ({ focused, color }: any) => {
-            let iconName: any;
-
-            if (route.name === "Home") {
-              iconName = focused ? "ios-restaurant" : "ios-restaurant-outline";
-            } else if (route.name === "Favorites") {
-              iconName = focused ? "ios-star" : "ios-star-outline";
-            }
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={20} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={AuthMealsNavigator}
-          options={{
-            tabBarColor: colors.primaryColor,
-          }}
-        />
-        <Tab.Screen
-          name="Favorites"
-          component={FavNavigator}
-          options={{
-            tabBarColor: colors.accentColor,
-          }}
-        />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="MealsFavs" component={MealsTab}></Drawer.Screen>
+        <Drawer.Screen name="Filters" component={FilterNav}></Drawer.Screen>
+      </Drawer.Navigator>
     </LoadAssets>
   );
 }
