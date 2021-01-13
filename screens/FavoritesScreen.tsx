05895c66c003196cfec0958a084b8cc2ee4cec61
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-13 06:21:56 +0200

    render feedback in favscreen

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Text, View } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 
@@ -24,6 +25,16 @@ const FavoritesScreen = (props: any) => {
   }, [setOptions]);
 
   const favMeals = useSelector((state: any) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontFamily: "regular", fontSize: 16 }}>
+          No favorite meals found. Start adding some!
+        </Text>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
